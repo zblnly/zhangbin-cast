194bae1cecff898a3fc2ba8d172fa079e6ae4ea5
zblnly <zblnly@users.noreply.github.com>
2026-06-04 23:01:40 +0800

    Auto-fix: DiscoveryService.kt

--- tv-app/app/src/main/java/com/zhangbin/cast/tv/DiscoveryService.kt
+++ tv-app/app/src/main/java/com/zhangbin/cast/tv/DiscoveryService.kt
@@ -76,7 +76,7 @@ class DiscoveryService : Service() {
 
     private fun startUdpBroadcast() {
         isRunning = true
-        broadcastThread = Thread(name = "udp-broadcast") {
+        broadcastThread = Thread {
             try {
                 val message = "ZHANGBIN_CAST|$DEVICE_NAME|$SIGNALING_PORT|1.0"
                 val data = message.toByteArray()
@@ -103,7 +103,7 @@ class DiscoveryService : Service() {
             } catch (e: Exception) {
                 Log.e(TAG, "Broadcast thread error", e)
             }
-        }.apply { isDaemon = true }.start()
+        }.apply { name = "udp-broadcast"; isDaemon = true }.start()
 
         Log.i(TAG, "UDP broadcast started on $MULTICAST_ADDR:$MULTICAST_PORT")
     }
